f67ab4c0fe0be39ac64c2367d836069e463a32d8
MikeSimulation <jordan.giffenhype@gmail.com>
2020-05-06 21:36:17 +0100

    237. Setup Track Throttles

--- BattleTank/Source/BattleTank/Projectile.cpp
+++ BattleTank/Source/BattleTank/Projectile.cpp
@@ -30,8 +30,6 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::LaunchProjectile(float Speed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%f: Projectile fires at speed: %f"), GetWorld()->GetTimeSeconds(), Speed);
-
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
