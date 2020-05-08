ede900fc86471a341b37c2dbfa2e229a83f8f0f5
MikeSimulation <jordan.giffenhype@gmail.com>
2020-05-08 23:14:48 +0100

    249. How to Use Blueprint Variables

--- BattleTank/Source/BattleTank/BattleTank_PlayerController.h
+++ BattleTank/Source/BattleTank/BattleTank_PlayerController.h
@@ -14,12 +14,14 @@ class BATTLETANK_API ABattleTank_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
 	ATank* GetControlledTank() const;
 
 private:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects with the world
 	void AimTowardsCrosshair();
