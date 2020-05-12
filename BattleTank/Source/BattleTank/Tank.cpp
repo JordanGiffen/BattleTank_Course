de669268de2fe1147c8c53e1cb03fcfcc14957b9
MikeSimulation <jordan.giffenhype@gmail.com>
2020-05-12 01:40:50 +0100

    280. BlueprintPure & Health Bars

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -28,3 +28,8 @@ float ATank::TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEv
 	
 	return DamageToApply;
 }
+
+float ATank::GetHealthPercent() const
+{
+	return (float)CurrentHealth / (float)StartingHealth;
+}
